574d5f2fa77d37695d8e531dae086e08a3343b06
UTKARSH SAINI <utkarshsaini19@gmail.com>
2023-04-15 01:10:04 +0530

    Changed an API
    Changed an API

--- pages/api/getblog.js
+++ pages/api/getblog.js
@@ -1,16 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 // http://localhost:3000/api/getblog?slug=how-to-learn-javascript
 
-import type { NextApiRequest, NextApiResponse } from 'next'
+
 import * as fs from 'fs'
 
-type Data = {
-    error: string
-}
+
 
 export default function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<Data>
+    req,res
 ) {
     fs.readFile(`blogdata/${req.query.slug}.json`, 'utf-8', (error, data) => {
         console.log(req.query.slug);
